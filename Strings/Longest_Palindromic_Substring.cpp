43ce733b230c7c8707ed64942f88640fec7b0b5e
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-06-21 13:27:14 +0530

    Longest Palindromic Substring

--- Strings/Longest_Palindromic_Substring.cpp
+++ Strings/Longest_Palindromic_Substring.cpp
@@ -15,73 +15,139 @@
 
 string longestPalindrome(string s) {
 
-        int start = 0 , index = 0 ,length=0;
+        int m=0,l=0,r=0;
+        int length=0,index=0;
 
-        while(start<s.size())
+        while(m<s.size())
         {
-
-            string str1,str2;
-
-            for(int i = start;i<s.size();i++)
+            if(l>=0 && r<s.size() && s[l]==s[r])
             {
-                str1+=s[i];
-            }
-
-            if(str1.size()<length){
-                break;
-            }
-
-            str2=str1;
-            reverse(str2.begin(),str2.end());
-
-            str1=str1+'$'+str2;
-
-            int first=0,second=1;
-
-            vector<int> lps(str1.size(),0);
-
-            while(second<str1.size())
-            {
-                if(str1[second]==str1[first])
+                if(length<(r-l+1))
                 {
-                    lps[second]=first+1;
-                    first++;
-                    second++;
-                }
-                else
-                {
-                    if(first==0)
-                    {
-                        second++;
-                    }
-                    else
-                    {
-                        first=lps[first-1];
-                    }
-
-                }
+                    length=r-l+1;
+                    index=l;
+                } 
+                l--;
+                r++; 
             }
-                
-            cout<<lps[lps.size()-1];
-
-            if(length<lps[lps.size()-1])
+            else
             {
-                length=lps[lps.size()-1];
-                index=start;
+                m++;
+                l=m;
+                r=m;
             }
-
-            start++;
-
-
         }
 
-        string ans;
+        m=0;
+        l=0;
+        r=m+1;
 
-        for(int i=index;i<index+length;i++)
+        while(m<s.size())
+        {
+            if(l>=0 && r<s.size() && s[l]==s[r])
+            {
+                if(length<(r-l+1))
+                {
+                    length=r-l+1;
+                    index=l;
+                } 
+                l--;
+                r++; 
+            }
+            else
+            {
+                m++;
+                l=m;
+                r=m+1;
+            }
+        }
+
+        string ans="";
+
+        for(int i = index;i<index+length;i++)
         {
             ans+=s[i];
         }
 
         return ans;
-        
+          
     }
+
+
+// too bad thought
+// string longestPalindrome(string s) {
+
+//         int start = 0 , index = 0 ,length=0;
+//         string str1,str2;
+//         vector<int> lps;
+//         int first,second;
+
+//         while(start<s.size())
+//         {
+
+            
+//             str1="";
+//             str2="";
+//             for(int i = start;i<s.size();i++)
+//             {
+//                 str1+=s[i];
+//             }
+
+//             if(str1.size()<length){
+//                 break;
+//             }
+            
+//             str2=str1;
+//             reverse(str2.begin(),str2.end());
+
+//             str1=str1+'$'+str2;
+
+//             first=0,second=1;
+
+//             lps.resize(str1.size());
+
+//             while(second<str1.size())
+//             {
+//                 if(str1[second]==str1[first])
+//                 {
+//                     lps[second]=first+1;
+//                     first++;
+//                     second++;
+//                 }
+//                 else
+//                 {
+//                     if(first==0)
+//                     {   
+//                         lps[second]=0;
+//                         second++;
+//                     }
+//                     else
+//                     {
+//                         first=lps[first-1];
+//                     }
+
+//                 }
+//             }
+                
+
+//             if(length<lps[lps.size()-1])
+//             {
+//                 length=lps[lps.size()-1];
+//                 index=start;
+//             }
+
+//             start++;
+
+
+//         }
+
+//         string ans;
+
+//         for(int i=index;i<index+length;i++)
+//         {
+//             ans+=s[i];
+//         }
+
+//         return ans;
+        
+//     }
