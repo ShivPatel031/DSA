6f91b5424db7a5bdb9129a7eaa7a7e305e7fa44c
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-01 22:24:29 +0530

    Time Needed to Buy Tickets

--- Queue/Time_Needed_to_Buy_Tickets.cpp
+++ Queue/Time_Needed_to_Buy_Tickets.cpp
@@ -33,6 +33,22 @@
 // 1 <= tickets[i] <= 100
 // 0 <= k < n
 
+int timeRequiredToBuy(vector<int>& tickets, int k) 
+ {
+     int time = 0;
+
+     for(int i = 0 ; i<=k ;i++)
+     {
+         time+=min(tickets[k],tickets[i]);
+     }
+
+     for(int i = k+1 ; i<tickets.size() ;i++)
+     {
+         time+=min(tickets[k]-1,tickets[i]);
+     }
+
+     return time;
+ }
 
 // but below code take O(N^2)
 // int timeRequiredToBuy(vector<int>& tickets, int k) 
