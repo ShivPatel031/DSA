ad9b0730b4d7566871cb5c81cce3e6f02ef4ddfa
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-02 08:33:05 +0530

    Implement Queue using Stacks

---
+++ Queue/Implement_Queue_using_Stacks.cpp
@@ -0,0 +1,55 @@
+// Implement Queue using Stacks leetcode 232 GFG
+
+class MyQueue 
+{
+public:
+    stack<int> s1,s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+
+        if(empty())
+        {
+            return -1;
+        }
+
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int element = s2.top();
+        s2.pop();
+        return element;
+    }
+    
+    int peek() {
+        if(empty())
+        {
+            return -1;
+        }
+        
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
