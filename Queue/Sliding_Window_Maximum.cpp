1ac6186d7ccfa1b233f26da80792bc91a5cfc2ff
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-02 15:00:38 +0530

    Sliding Window Maximum

---
+++ Queue/Sliding_Window_Maximum.cpp
@@ -0,0 +1,66 @@
+// Sliding Window Maximum leetcode 239
+
+// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+// Example 1:
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Output: [3,3,5,5,6,7]
+// Explanation: 
+// Window position                Max
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+  
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// 1 <= k <= nums.length
+
+
+// timecomplexsity O(N)
+vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+{
+    vector<int> ans;
+    deque<int> d;
+
+    for (int i = 0; i < k - 1; i++) 
+    {
+        while (!d.empty() && nums[d.back()] < nums[i]) 
+        {
+            d.pop_back();
+        }
+
+        d.push_back(i);
+    }
+
+    for (int i = k - 1; i < nums.size(); i++) 
+    {
+        if (!d.empty() && d.front() <= i - k) 
+        {
+            d.pop_front();
+        }
+
+        while (!d.empty() && nums[d.back()] < nums[i]) 
+        {
+            d.pop_back();
+        }
+
+        d.push_back(i);
+        ans.push_back(nums[d.front()]);
+    }
+
+    return ans;
+}
