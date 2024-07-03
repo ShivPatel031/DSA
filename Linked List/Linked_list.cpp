a3d7a32ae1a6267aa787fc85dde1ddf1bfd25983
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-03 10:34:33 +0530

    Linked list

---
+++ Linked List/Linked_list.cpp
@@ -0,0 +1,26 @@
+class Node
+{
+  public:
+  int data;
+  Node *next;
+
+  Node()
+  {
+    next=nullptr;
+  }
+
+  Node(int x)
+  {
+    data=x;
+    next=nullptr;
+  }
+};
+
+class LinkeList
+{
+Node *head;
+public:
+
+void createNode()
+
+};
