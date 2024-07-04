4d4be8e3639c053fa772b38981548264c296434c
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-04 07:48:03 +0530

    Linked List

--- Linked List/Linked_list.cpp
+++ Linked List/Linked_list.cpp
@@ -1,26 +1,96 @@
 class Node
 {
-  public:
-  int data;
-  Node *next;
-
-  Node()
-  {
-    next=nullptr;
-  }
-
-  Node(int x)
-  {
-    data=x;
-    next=nullptr;
-  }
+    public:
+    int data;
+    Node *next;
+  
+    Node()
+    {
+      next=nullptr;
+    }
+  
+    Node(int x)
+    {
+      data=x;
+      next=nullptr;
+    }
 };
 
 class LinkeList
 {
-Node *head;
-public:
+    Node *head,*tail;
+    int size;
+    public:
 
-void createNode()
+
+    LinkeList()
+    {
+      head=nullptr;
+      tail=nullptr;
+      size=0;
+    }
+
+
+    void addAtFront(int x)
+    {
+      if(head==nullptr)
+      {
+        head=new Node(x);
+        tail=head;
+      }
+      else
+      {
+        Node *temp = new Node(x);
+        temp->next=head;
+        head=temp;
+      }
+      size++;
+    }
+
+
+    void addAtBack(int x)
+    {
+      if(head==nullptr)
+      {
+        head=new Node(x);
+        tail=head;
+      }
+      else
+      {
+        Node *temp = new Node(x);
+        tail->next=temp;
+        tail=temp;
+      }
+      size++;
+    }
+
+
+    void addAfterIndex(int x,int index)
+    {
+        if(head==nullptr)
+        {
+          head=new Node(x);
+          tail=head;
+          cout<<"Given index does not exist";
+        }
+        else
+        {
+          index--;
+          Node *temp = head;
+          while(index--)
+          {
+            if(!temp->next)
+            {
+              cout<<"Index does not exist";
+              break;
+            }
+            temp=temp->next;
+          }
+          Node *newNode = new Node(x);
+          newNode->next=temp->next;
+          temp->next=newNode;
+        }
+        size++;
+    }
 
 };
