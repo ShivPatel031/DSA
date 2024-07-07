a6944a9cde662c6c38cf73be7c3a18b82d0a2f8b
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-07 15:06:50 +0530

    Reverse Nodes in k-Group

--- Linked List/Reverse_Nodes_in_k-Group.cpp
+++ Linked List/Reverse_Nodes_in_k-Group.cpp
@@ -22,6 +22,40 @@
 // 1 <= k <= n <= 5000
 // 0 <= Node.val <= 1000
 
+// other and more * optimize way
+ListNode* reverseKGroup(ListNode* head, int k) 
+{
+    if (!head || k == 1) {
+        return head;
+    }
+
+    int len = 0;
+    ListNode* curr = head;
+
+    while(curr) {
+        curr = curr->next;
+        len++;
+    }
+
+    ListNode* dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode* prev = dummy;
+
+    while (len >= k) {
+        curr = prev->next;
+        for (int i = 1; i < k; i++) {
+            ListNode* next_node = curr->next;
+            curr->next = next_node->next;
+            next_node->next = prev->next;
+            prev->next = next_node;
+        }
+        prev = curr;
+        len -= k;
+    }
+    return dummy->next;
+}
+
+// work
 ListNode* reverseKGroup(ListNode* head, int k) 
 {
     ListNode *temp=head,*temp2,*temp3,*past1;
