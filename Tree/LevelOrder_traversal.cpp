c80247ccb8b854e146d48fceca60adb5735564d0
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-11 13:42:20 +0530

    Level Order traversal

--- Tree/LevelOrder_traversal.cpp
+++ Tree/LevelOrder_traversal.cpp
@@ -1,20 +1,26 @@
 vector<int> levelOrder(Node *r){
 
-        queue<Node *> q;
-        q.push(r);
-
-        vector<int> ans
-;
-        while (!q.empty()){
-
-            Node *temp=q.front();
+    vector<int> ans;
+    queue<Node *> q;
+    q.push(root);
+    int count=0;
+    
+    while(!q.empty())
+    {
+        count=q.size();
+            
+        while(count--)
+        {
+            Node *temp =q.front();
             q.pop();
-
+            
             ans.push_back(temp->data);
-
-            if (temp->left) q.push(temp->left);
-            if (temp->right) q.push(temp->right);
+            
+            if(temp->left) q.push(temp->left);
+            
+            if(temp->right) q.push(temp->right);
         }
+    }
 
-        return ans;
+ return ans;
 }
