207f5b31e67f70c466e5ab6ec0dc08977a34b271
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-12 17:10:01 +0530

    PostOrder Traversal

--- Tree/PostOrder_traversal.cpp
+++ Tree/PostOrder_traversal.cpp
@@ -1,49 +1,41 @@
 // using recursion
-void postorder_recursive(Node *r){
-
-        if (r == nullptr){
-            return;
-        }
-
-        // NLR
-
-        cout<<r->data<<" ";
-
-        postorder(r->left);
+void postorder(Node *root,vector<int> &ans)
+{
+    if(!root)  return;
         
-        postorder(r->right);
-    }
-
+    postorder(root->left,ans);
+    postorder(root->right,ans);
+    ans.push_back(root->data);
+}
+vector <int> postOrder(Node* root)
+{
+    vector<int> ans;
+    postorder(root,ans);
+    return ans;
+}
 // using iteration
-void postorder_iteration(Node *r){
-
-        vector<int> ans;
-        stack<Node *> s;
-
-        s.push(root);
-
-        while (!s.empty()){
-
-            Node *temp=s.top();
-            s.pop();
-
-            ans.push_back(temp->data);
-
-            if (temp->left) s.push(temp->left);
-            if (temp->right) s.push(temp->right);  // we add right Node last because then Right Node came on top
-        }
-        // we got ans but in reverse
-
-        //to reverse ans 
-        for (int i=0;i<ans.size()/2;i++){
-
-            int temp=ans[i];
-            ans[i]=ans[ans.size()-1-i];
-            ans[ans.size()-1-i]=temp;
-        }
-
-        //display result
-        for (auto it : ans){
-            cout<<it<< " ";
-        }
+vector<int> postOrder(Node* root) 
+{
+    vector<int> ans;
+    if(!root) return ans;
+    
+    stack<Node *> s;
+    s.push(root);
+    
+    while(!s.empty())
+    {
+        Node *temp =s.top();
+        s.pop();
+        
+        ans.push_back(temp->data);
+        
+        if(temp->left) s.push(temp->left);
+        
+        if(temp->right) s.push(temp->right);
     }
+    
+    reverse(ans.begin(),ans.end());
+    
+    return  ans;
+    
+}
