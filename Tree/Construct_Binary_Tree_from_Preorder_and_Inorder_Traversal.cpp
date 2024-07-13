eb5c56a3982804f1e010cd44cc2bf2f4419667d7
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-07-13 12:19:26 +0530

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Tree/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,76 @@
+// Construct Binary Tree from Preorder and Inorder Traversal leetcode 105 GFG
+
+TreeNode* createTree(vector<int> &arr,vector<int> &preorder,int index,int start,int end)
+{
+    int pos=-1;
+    
+    for(int i = start;i<end;i++)
+    {
+        if(preorder[index]==arr[i]) pos=i;
+    }
+    
+    if(pos==-1) return nullptr;
+     
+    TreeNode *temp=new TreeNode(preorder[index]);
+    
+    temp->left=createTree(arr,preorder,index+1,start,pos);
+    temp->right=createTree(arr,preorder,index + (pos-start)+1,pos+1,end);
+    
+    return temp;
+    
+}
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
+{
+    return createTree(inorder,preorder,0,0,inorder.size());
+}
+
+// what i first thought
+// TreeNode* createTree(vector<int> arr,vector<int> &inorder,int index)
+// {
+//     if(!arr.size())
+//     {
+//         return nullptr;
+//     }
+    
+//     int pos=-1;
+    
+//     for(int i = 0;i<arr.size();i++)
+//     {
+//         if(inorder[index]==arr[i])
+//         {
+//             pos=i;
+//         }
+//     }
+    
+//     if(pos==-1)
+//     {
+//         return nullptr;
+//     }
+    
+//     vector<int> arr1;
+//     vector<int> arr2;
+    
+//     for(int i = 0 ; i<arr.size();i++)
+//     {
+        
+//         if(i<pos)
+//         {
+//             arr1.push_back(arr[i]);
+//         }       
+//         else if(i>pos)
+//         {
+//             arr2.push_back(arr[i]);
+//         }
+//     }
+    
+//     TreeNode *temp=new TreeNode(inorder[index]);
+    
+//     temp->left=createTree(arr1,inorder,index+1);
+//     temp->right=createTree(arr2,inorder,index + pos+1);
+    
+//     return temp;
+    
+// }
+// TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//     return createTree(inorder,preorder,0);
+// }
