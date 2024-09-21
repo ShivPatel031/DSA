57d311b0d70a0bac020aea860f98702c9215d4c9
ShivPatel031 <113342110+ShivPatel031@users.noreply.github.com>
2024-09-21 16:21:17 +0530

    Fibonacci Number

--- Dynamic Programming/Fibonacci_Number.cpp
+++ Dynamic Programming/Fibonacci_Number.cpp
@@ -60,3 +60,23 @@ int fib(int n)
 
     return dp[n];
 }
+
+
+// bottom up with space optimization
+int fib(int n) 
+{
+    if(n<=1) return n;
+
+    vector<int> dp(3,-1);
+    dp[0]=0;
+    dp[1]=1;
+
+    for(int i = 2 ; i < n+1 ; i++)
+    {
+        dp[2]=dp[1]+dp[0];
+        dp[0]=dp[1];
+        dp[1]=dp[2];
+    }
+
+    return dp[2];
+}
